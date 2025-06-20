f5cf1b205392f128f9b5b269771358e050b20959
VallNB <leo.dalgarno@roboroos.org.au>
2025-06-20 04:58:18 +0000

    [automatic commit] 2025-06-20 04:58:18 UTC 123.136.53.158

--- part1/app.js
+++ part1/app.js
@@ -13,7 +13,8 @@ var app = express();
     try {
         var dbConnectionPool = mysql.createPool({ database: 'pokepare', user: 'root' });
 
-        var exist = dbConnectionPool.query("INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 1)");
+        var exist = dbConnectionPool.query("SELECT * FROM Users");
+        if (extist )
         await dbConnectionPool.query("INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 1)");
         await dbConnectionPool.query("INSERT INTO Users (username, email, password_hash, role) VALUES ('bobwalker', 'bob@example.com', 'hashed456', 2)");
 
