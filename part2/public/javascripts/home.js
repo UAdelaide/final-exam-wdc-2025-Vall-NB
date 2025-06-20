c0d7293dc74c80bd1bec148e07eb9354cf0a8fa8
VallNB <leo.dalgarno@roboroos.org.au>
2025-06-20 05:45:16 +0000

    [automatic commit] 2025-06-20 05:45:16 UTC 123.136.53.158

--- part2/public/javascripts/home.js
+++ part2/public/javascripts/home.js
@@ -1,3 +1,13 @@
 function login() {
-    
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+
+        }
+    };
+
+    xhttp.open("POST", "/actrmv", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify({username:sacrifice}));
 }
