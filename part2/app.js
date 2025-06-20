362e7dbdb41002e46e71419bcdbace96f1db33e6
VallNB <leo.dalgarno@roboroos.org.au>
2025-06-20 23:39:47 +0000

    [automatic commit] 2025-06-20 23:39:47 UTC 123.136.53.158

--- part2/app.js
+++ part2/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
-//var mysql = require('mysql2');
+var mysql = require('mysql2');
 require('dotenv').config();
 
 const app = express();
