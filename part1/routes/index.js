ace2a608fa02d981b171e82ca348691f4e0b874f
VallNB <leo.dalgarno@roboroos.org.au>
2025-06-20 05:25:58 +0000

    [automatic commit] 2025-06-20 05:25:58 UTC 123.136.53.158

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -59,7 +59,7 @@ router.get('/api/walkers/summary', function(req, res) {
         res.sendStatus(500);
         return;
       }
-      connection.execute(``, function (error, results, fields) {
+      connection.execute(`SELECT AVG()`, function (error, results, fields) {
       connection.release();
         if (error) throw error;
           res.send(results);
