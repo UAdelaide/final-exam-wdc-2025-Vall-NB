5c562d4f294cd16ab63d16745d64e9e567d0e66e
VallNB <leo.dalgarno@roboroos.org.au>
2025-06-20 07:19:03 +0000

    [automatic commit] 2025-06-20 07:19:02 UTC 123.136.53.158

--- part2/public/javascripts/nothome.js
+++ part2/public/javascripts/nothome.js
@@ -11,5 +11,14 @@ function logout() {
 }
 
 function optiongen() {
-    
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            window.location.replace("http://localhost:8080/");
+        }
+    };
+    xhttp.open("GET", "/logout", true);
+    xhttp.send();
+
 }
