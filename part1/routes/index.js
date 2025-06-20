710820a9e1e63717354ceaca884973271b622259
VallNB <leo.dalgarno@roboroos.org.au>
2025-06-20 05:22:49 +0000

    [automatic commit] 2025-06-20 05:22:49 UTC 123.136.53.158

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -59,9 +59,7 @@ router.get('/api/walkrequests/open', function(req, res) {
         res.sendStatus(500);
         return;
       }
-      connection.execute(`SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location,
-      Users.username AS owner_username FROM ((WalkRequests INNER JOIN Dogs ON Dogs.dog_id= WalkRequests.dog_id)
-      INNER JOIN Users ON Users.user_id  = Dogs.owner_id) WHERE WalkRequests.status = 1`, function (error, results, fields) {
+      connection.execute(``, function (error, results, fields) {
       connection.release();
         if (error) throw error;
           res.send(results);
