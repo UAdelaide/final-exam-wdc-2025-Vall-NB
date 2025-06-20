53d003cadb384fd0e779826f4a92df7a630799eb
VallNB <leo.dalgarno@roboroos.org.au>
2025-06-20 05:56:07 +0000

    [automatic commit] 2025-06-20 05:56:07 UTC 123.136.53.158

--- part2/public/javascripts/home.js
+++ part2/public/javascripts/home.js
@@ -1,8 +1,11 @@
+const { json } = require("express");
+
 function login() {
     var xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
+            var data = json.parse()
             // Redirect
         }
     };
