e37d87ca93d1b05df0e6d3ba13c523ab4384cc6a
VallNB <leo.dalgarno@roboroos.org.au>
2025-06-20 06:27:27 +0000

    [automatic commit] 2025-06-20 06:27:27 UTC 123.136.53.158

---
+++ part2/public/javascripts/nothome.js
@@ -0,0 +1,23 @@
+function logout() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            var data = json.parse(this.responseText);
+            // Checks if dogwalker
+            if (data[0].role === 1) {
+                // Redirects page
+                location.replace("http://localhost:8080/owner-dashboard.html");
+            } else {
+                location.replace("http://localhost:8080/walker-dashboard.html");
+            }
+        }
+    };
+    xhttp.open("POST", "/login", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    // Gets email and password from index page
+    var loginmail = document.getElementById('Lemail').value;
+    var loginpass = document.getElementById('Lpassword').value;
+
+    xhttp.send(JSON.stringify({email: loginmail, password: loginpass}));
+}
