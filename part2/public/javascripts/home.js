36fb5b195ceb95d04a85d763886e6eb7611528d2
VallNB <leo.dalgarno@roboroos.org.au>
2025-06-20 06:17:55 +0000

    [automatic commit] 2025-06-20 06:17:55 UTC 123.136.53.158

--- part2/public/javascripts/home.js
+++ part2/public/javascripts/home.js
@@ -15,10 +15,11 @@ function login() {
             }
         }
     };
-    console.log(document.getElementById('Lemail').value);
     xhttp.open("POST", "/login", true);
     xhttp.setRequestHeader("Content-type", "application/json");
     // Gets email and password from index page
     var loginmail = document.getElementById('Lemail').value;
-    xhttp.send(JSON.stringify({email: document.getElementById('Lemail').value, password: document.getElementById('Lpassword').value}));
+    var loginpass = document.getElementById('Lpassword').value;
+
+    xhttp.send(JSON.stringify({email: loginmail, password: loginpass}));
 }
