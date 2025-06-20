4e20c4ef519060a59c513074aa8f2b157c4c357c
VallNB <leo.dalgarno@roboroos.org.au>
2025-06-20 03:30:30 +0000

    [automatic commit] 2025-06-20 03:30:30 UTC 123.136.53.158

--- part1/public/javascripts/dotd.js
+++ part1/public/javascripts/dotd.js
@@ -1,18 +1,9 @@
 function debut() {
-        var xhttp = new XMLHttpRequest();
+    var xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            if (this.responseText != "") {
-                //console.log(this.responseText)
-                var data = JSON.parse(this.responseText);
-                //var data = this.responseText;
-                //console.log(data.results[Math.floor(Math.random()*1025)].name);
-                var qmon = data.results[Math.floor(Math.random()*1025)].name;
-                callback(qmon);
-                callback2(qmon);
-                callback3();
-            }
+            document.getElementById('')
         }
 
     };
