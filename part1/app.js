38105c0b497e423269ff60dd259b1cd414ca8af5
VallNB <leo.dalgarno@roboroos.org.au>
2025-06-20 05:10:12 +0000

    [automatic commit] 2025-06-20 05:10:12 UTC 123.136.53.158

--- part1/app.js
+++ part1/app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+/*
 let dbConnectionPool;
 (async () => {
     try {
@@ -26,6 +26,7 @@ let dbConnectionPool;
         console.error("Error setting up database.");
     }
 })();
+*/
 
 var dbConnectionPool2 = mysql.createPool({ database: 'DogWalkService', user: 'root' });
 app.use(function(req, res, next) {
