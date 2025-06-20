c0a8a1f852be6186d5d4c1a3aabd514af8077822
VallNB <leo.dalgarno@roboroos.org.au>
2025-06-20 03:47:17 +0000

    [automatic commit] 2025-06-20 03:47:16 UTC 123.136.53.158

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/api/dogs, function(req, res) {
+    req.pool.getConnection(function(err,connection) {
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      connection.execute('SELECT name, size, username  FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id', function (error, results, fields) {
+      connection.release();
+        if (error) throw error;
+          res.send(results);
+
+      });
+    });
+});
+
+
 module.exports = router;
