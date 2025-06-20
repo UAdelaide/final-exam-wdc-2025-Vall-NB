64fea65f6ef6bd4955b349b0ff9decbb7343036e
VallNB <leo.dalgarno@roboroos.org.au>
2025-06-20 08:16:46 +0000

    [automatic commit] 2025-06-20 08:16:45 UTC 123.136.53.158

--- part2/public/javascripts/home.js
+++ part2/public/javascripts/home.js
@@ -38,10 +38,11 @@ function tablegen() {
                 document.getElementById('dog_id').innerHTML += rowstr;
                     var xhttp = new XMLHttpRequest();
 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-        }
-    };
+                    xhttp.onreadystatechange = function() {
+                        if (this.readyState === 4 && this.status === 200) {
+
+                        }
+                    };
     xhttp.open("GET", "/api/dogs", true);
     xhttp.send();
 
