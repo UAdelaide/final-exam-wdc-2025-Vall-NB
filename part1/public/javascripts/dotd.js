ae9fff53b10987e590e43509417ab57c3e8827f7
VallNB <leo.dalgarno@roboroos.org.au>
2025-06-20 03:30:09 +0000

    [automatic commit] 2025-06-20 03:30:09 UTC 123.136.53.158

--- part1/public/javascripts/dotd.js
+++ part1/public/javascripts/dotd.js
@@ -1,3 +1,23 @@
 function debut() {
-    
+        var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            if (this.responseText != "") {
+                //console.log(this.responseText)
+                var data = JSON.parse(this.responseText);
+                //var data = this.responseText;
+                //console.log(data.results[Math.floor(Math.random()*1025)].name);
+                var qmon = data.results[Math.floor(Math.random()*1025)].name;
+                callback(qmon);
+                callback2(qmon);
+                callback3();
+            }
+        }
+
+    };
+
+    xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon?limit=1025&offset=0", true);
+    xhttp.send();
+
 }
