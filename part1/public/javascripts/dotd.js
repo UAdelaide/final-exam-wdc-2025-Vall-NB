8d99d617fb1132764d5a9824505454cf4b138229
VallNB <leo.dalgarno@roboroos.org.au>
2025-06-20 03:32:58 +0000

    [automatic commit] 2025-06-20 03:32:58 UTC 123.136.53.158

--- part1/public/javascripts/dotd.js
+++ part1/public/javascripts/dotd.js
@@ -2,7 +2,7 @@ function debut() {
     var xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             var res = JSON.parse(this.responseText);
             document.getElementById('dotd').src = res.message;
         }
